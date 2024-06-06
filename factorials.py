19229c2fd53bfcda5d097d0eab053b6611a462a8
Caio Freitas <caio.freitas@irede.org.br>
2024-06-06 12:50:09 -0300

    n input type and value validations

--- factorials.py
+++ factorials.py
@@ -1,6 +1,10 @@
-def factorial_calc(n):
-    if n == 0:
+def factorial_calc(n: int) -> int:
+    if n == 0 or n == 1:
         return 1
+    elif n < 0:
+        return "Factorial is not possible for negative numbers"
+    elif type(n) != int:
+        return "Factorial is not possible for non-integer numbers"
     return n * (factorial_calc(n - 1))
 
 n = input("Enter a number: ")
